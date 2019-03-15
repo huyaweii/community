c1bb2106e94666731d5a9af371557e9c207175a8
hyw <2409551912@qq.com>
2019-03-15 15:37:58 +0800

    icon由数据库保存

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,18 +2,9 @@ var app = getApp()
 import {asyncRequest} from '../../api'
 const regeneratorRuntime = require('../../utils/runtime.js')
 
-const servicesIcon = {
-  '房屋装修': 'repair',
-  '家电维修': 'decoration',
-  '开锁换锁': 'lock',
-  '宽带安装': 'broadband',
-  '成人保健': 'sex'
-}
-
 Page({
   data: {
-    services: [],
-    servicesIcon
+    services: []
   },
   onLoad: async function (options) {
     const _this = this
--- pages/list/index.js
+++ pages/list/index.js
@@ -5,12 +5,19 @@ const regeneratorRuntime = require('../../utils/runtime.js')
 Page({
   data: {
     shopkeepers: [],
+    loading: true
   },
   onLoad: async function (options) {
     const _this = this
     const {shopkeepers} = await asyncRequest({
       url: `/merchantService/${options.serviceId}/shopkeepers`,
     })
-    this.setData({shopkeepers})
+    this.setData({shopkeepers, loading: false})
+  },
+  call: function (e) {
+    const phoneNumber = e.currentTarget.dataset.tel
+    wx.makePhoneCall({
+      phoneNumber
+    })
   }
 })
